a84988aa2b51fe00072c63f9d61bdccba5486c60
Riki Montgomery <mostriki820@gmail.com>
2019-02-18 20:38:14 -0800

    call by reference

--- practice-problems/inches-to-millimeters/main.cpp
+++ practice-problems/inches-to-millimeters/main.cpp
@@ -2,22 +2,43 @@
 
 using namespace std;
 
-float convert(float inches){
-    return inches * 25.4;
-};
+//float convert(float inches){
+//    return inches * 25.4;
+//};
+//
+//int main(){
+//
+//    float in = 0;
+//    float mm = 0 ;
+//
+//    cout << "Enter the number of inches: ";
+//    cin >> in;
+//
+//    mm = convert(in);
+//
+//    cout << in << " inches converts to " << mm << "mm";
+//
+//    return 0;
+//}
 
-int main(){
+// call by reference example
 
-    float in = 0;
-    float mm = 0 ;
+void convert(float inches, float &mils);
+
+int main() {
+    float in;
+    float mm;
 
     cout << "Enter the number of inches: ";
     cin >> in;
 
-    mm = convert(in);
+    convert(in, mm);
 
     cout << in << " inches converts to " << mm << "mm";
 
     return 0;
 }
 
+void convert(float inches, float & mils) {
+    mils = 25.4 * inches;
+}
