0f03a8dca3f10c944b3d09378a1a34c0cb3c1531
Riki Montgomery <mostriki820@gmail.com>
2019-03-18 00:58:08 -0700

    damn

--- a08/main.cpp
+++ a08/main.cpp
@@ -16,19 +16,26 @@
 
 using namespace std;
 
+struct Temperature {
+    string month;
+    int highTemp;
+    int lowTemp;
+};
+
 void loadData(ifstream &infile, Temperature [], int &size);
 Temperature averageHigh(Temperature [], int size);
-Temperaure averageLow(Temperature [], int size);
+Temperature averageLow(Temperature [], int size);
 
 int main() {
     // input variable to operate on
     ifstream infile;
 
-    int rows = 12;
+    int size = 12;
     string filePath;
+    Temperature data[size];
     string month;
-    string months[rows];
-    int temp[rows][2];
+    int high = 0;
+    int low = 0;
 
     cout << "Enter the file path: \n";
     getline(cin, filePath);
@@ -41,10 +48,25 @@ int main() {
         throw runtime_error("Could not find file!");
     }
 
-    // while loop to read all lines of text from input file
-    loadData(infile, months, temp, rows);
+    loadData(infile, data, size);
 
+    cout << endl;
 
+    month = averageHigh(data, size).month;
+    high = averageHigh(data, size).highTemp;
+
+    cout << "At a scorching "
+         << high << " degrees Fahrenheit, "
+         << month << " was the hottest month on record." << endl;
+
+    cout << endl;
+
+    month = averageLow(data, size).month;
+    low = averageLow(data, size).lowTemp;
+
+    cout << "At a chilly "
+         << low << " degrees Fahrenheit, "
+         << month << " was the coolest month on record." << endl;
 
     infile.close();
 
@@ -58,8 +80,16 @@ int main() {
  * *    Description:    The function reads and stores data in the array of struct from a text file (temps.txt).
  * *
  *********************************************************************************************************************/
-void loadData(ifstream &infile, Temperature [], int &size) {
+void loadData(ifstream &infile, Temperature data[], int &size) {
+    // while loop to read all lines of text from input file
+    while (!infile.eof()) {
 
+        for (int i = 0; i < size; i++) {
+            infile >> data[i].month;
+            infile >> data[i].highTemp;
+            infile >> data[i].lowTemp;
+        }
+    }
 }
 
 /**********************************************************************************************************************
@@ -70,8 +100,20 @@ void loadData(ifstream &infile, Temperature [], int &size) {
  * *                    the high temperature and the corresponding month in main.
  * *
  *********************************************************************************************************************/
-void averageHigh(Temperature [], int size) {
+Temperature averageHigh(Temperature data[], int size) {
+    int temp = data[0].highTemp;
 
+    for(int i = 1; i < size; i++) {
+        if (data[i].highTemp > temp) {
+            temp = data[i].highTemp;
+        }
+    }
+
+    for(int j = 0; j < size; j++) {
+        if (data[j].highTemp == temp) {
+            return data[j];
+        }
+    }
 }
 
 /**********************************************************************************************************************
@@ -82,6 +124,18 @@ void averageHigh(Temperature [], int size) {
  * *                    low temperature and the corresponding month in main.
  * *
  *********************************************************************************************************************/
-void averageLow(Temperature [], int size) {
+Temperature averageLow(Temperature data[], int size) {
+    int temp = data[0].lowTemp;
 
+    for(int i = 1; i < size; i++) {
+        if (data[i].lowTemp < temp) {
+            temp = data[i].lowTemp;
+        }
+    }
+
+    for(int j = 0; j < size; j++) {
+        if (data[j].lowTemp == temp) {
+            return data[j];
+        }
+    }
 }
---
+++ in-class/wk09/practice-problem-03/main.cpp
@@ -0,0 +1,14 @@
+
+
+//Mod is back! Write a program to prompt a user for an integer and then pass the integer to a
+//function dividesSelf() which returns true if it divides itself, false otherwise.
+//We'll say that a positive int divides itself if every digit in the number divides
+//into the number evenly. So for example 128 divides itself since 1, 2, and 8 all
+//divide into 128 evenly. We'll say that 0 does not divide into anything evenly,
+//so no number with a 0 digit divides itself. Note: use % to get the
+//rightmost digit, and / to discard the rightmost digit. Add a loop in
+//main() to keep prompting the user until they are done.
+//
+//dividesSelf(128) → true
+//dividesSelf(12) → true
+//dividesSelf(120) → false
